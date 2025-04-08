7b6456083b90ee71ddb4454c68305ab026a90f47
Chethan R V <144924672+chethan-r30@users.noreply.github.com>
2025-04-08 12:43:24 +0530

    Validation err

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/WrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const {listingSchema} = require("./schema.js");
 const mongoose = require("mongoose");
 main().then(() =>
     console.log("connected to db"))
@@ -24,9 +25,18 @@ app.get("/", (req, res) => {
     res.send("hi i am root");
 });
 
+const validateListing =(req,res,next) =>{
+  let {error} = listingSchema.validate(req.body);
+  if(error){
+   throw new ExpressError(400,error);
+  }else{
+    next();
+  }
+};
+
 //index route
 
-app.get("/listings", wrapAsync(async (req,res)=>{
+app.get("/listings",validateListing, wrapAsync(async (req,res)=>{
   const alllistings = await Listing.find({});
   res.render("../views/listings/index.ejs",{alllistings});
 }));
@@ -40,11 +50,10 @@ app.get("/listings/new", (req,res)=>{
 
 app.post("/listings", wrapAsync(async(req,res,next)=>{
   //let{title,description,image,price,location,country}=req.body;
-    if(!req.body.listing){
-      throw new ExpressError(400,"send valid data for listing")
-    }
+  
     let listing = req.body.listing;
     const newListing =new Listing(listing) ;
+   
     await newListing.save();
     res.redirect("/listings");
    
@@ -57,11 +66,10 @@ app.get("/listings/:id/edit", wrapAsync( async (req,res)=>{
    const listing =await Listing.findById(id);
    res.render("../views/listings/edit.ejs",{listing});
 }));
+
+
 //update route
-app.put("/listings/:id", wrapAsync(async(req,res)=>{
-  if(!req.body.listing){
-    throw new ExpressError(400,"send valid data for listing")
-  }
+app.put("/listings/:id",validateListing, wrapAsync(async(req,res)=>{
     let {id} = req.params;
    await Listing.findByIdAndUpdate(id,{...req.body.listing});
    res.redirect(`/listings/${id}`);
@@ -82,6 +90,9 @@ app.delete("/listings/:id", wrapAsync(async (req,res) =>{
    console.log(deletedlisting);
    res.redirect("/listings");})
  );
+
+
+
 // app.get("/testlistings",async (req,res)=>{
 //     let samplelisting =new Listing({
 //         title:"chandu Villa",
@@ -103,7 +114,8 @@ app.all("*",(req,res,next)=>{
 app.use((err,req,res,next)=>{
   let{statusCode = 500,message="something went wrong"}= err;   //default values
   //res.send("Something went wrong!!");
-  res.status(statusCode).send(message);
+  // res.status(statusCode).send(message);
+  res.status(statusCode).render("error.ejs",{message});
 });
 
 
