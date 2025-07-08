39048dad5197e63e7891e364444c602797985d95
Chethan R V <144924672+chethan-r30@users.noreply.github.com>
2025-07-09 01:20:45 +0530

    auth

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const userRouter = require("./routes/user.js");
+const reviewRouter = require("./routes/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport=require("passport");
+const LocalStrategy=require("passport-local");
+const User = require("./models/user.js");
 const mongoose = require("mongoose");
 main()
   .then(() => console.log("connected to db"))
@@ -41,6 +45,11 @@ app.get("/", (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next)=>{
   res.locals.success = req.flash("success");
@@ -48,9 +57,19 @@ app.use((req,res,next)=>{
   next();
 });
 
+// app.get("/demouser",async(req,res)=>{
+// let fakeuser = new User({
+//   email:"student12@gmail.com",
+//   username:"delta-student"
+// });
+// let registeredUser = await User.register(fakeuser,"helloworld");
+// res.send(registeredUser);
+// });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/",userRouter);
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not found!"));
---
+++ routes/user.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const WrapAsync = require("../utils/WrapAsync");
+
+
+
+router.get("/signup",(req,res)=>{
+    res.render("users/signup.ejs");
+});
+
+router.post("/signup", WrapAsync(async(req,res)=>{
+    try{
+       let {username,email,password} = req.body;
+    const newUser = new User({email,username});
+   const registeredUser= await User.register(newUser,password);
+   console.log(registeredUser);
+   req.flash("success","Welcome to wanderlust");
+   res.redirect("/listings");
+    }catch(e){
+        req.flash("error",e.message);
+        res.redirect("/signup")
+    }
+    
+})
+);
+
+
+
+ module.exports=router;
