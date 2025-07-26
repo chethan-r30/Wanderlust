3039e11901239bcd710daa040cd120df209eef66
Chethan R V <144924672+chethan-r30@users.noreply.github.com>
2025-07-26 12:08:22 +0530

    Mongo-Atlas

--- app.js
+++ app.js
@@ -13,16 +13,19 @@ const listingRouter = require("./routes/listing.js");
 const userRouter = require("./routes/user.js");
 const reviewRouter = require("./routes/review.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport=require("passport");
 const LocalStrategy=require("passport-local");
 const User = require("./models/user.js");
 const mongoose = require("mongoose");
+
+const dbUrl = process.env.ATLASDB_URL;
 main()
   .then(() => console.log("connected to db"))
   .catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+  await mongoose.connect(dbUrl);
 }
 
 app.set("view engine", "ejs");
@@ -32,8 +35,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+
+const store = MongoStore.create({
+   mongoUrl:dbUrl,
+   crypto:{
+    secret:process.env.SECRET,
+   },
+   touchAfter:24*3600,
+});
+store.on("error",()=>{
+  console.log("Error in MONGO SESSION STORE",err);
+});
+
 const sessionOptions = {
-  secret: "mysupersecretcode",
+  store,
+  secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -46,6 +62,7 @@ const sessionOptions = {
 // app.get("/", (req, res) => {
 //   res.send("hi i am root");
 // });
+
 
 
 app.use(session(sessionOptions));
